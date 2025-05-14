91c4f38ac30b11bd171f168ef65f4ca221aa9d0c
luozhenyu0911 <1018954087@qq.com>
2025-05-14 14:31:49 +0800

    Revert "modify scripts"
    This reverts commit 38ddd080ad879ba238a40d16202f04d429b2e250.

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-# yhbatch -N 1 -n 64 -p mars
-# yhbatch -p e9 -n 1
 
-smk=/XYFS01/HDD_POOL/gzfezx_shhli/gzfezx_shhlixy_2/BIGDATA2/gzfezx_shhli_2/USER/luozhenyu/script/mitoquest_smk/smkfile/run.all.smk
+conda activate /home-02/zhenyuluo/anaconda3/envs/RNA-seq
+
+smk=./run.all.smk
 echo ""
 echo "start = $(date)"
 echo "$(hostname)"
-snakemake=/HOME/gzfezx_shhli/gzfezx_shhlixy_2/BIGDATA2/gzfezx_shhli_2/miniconda3/envs/cnvnator/bin/snakemake
-$snakemake -c 64 -pk -s ${smk} 2> snakemake.err.txt
+snakemake -j 80 -pk -s ${smk} 2> snakemake.err.txt
 echo "end = $(date)"
 # echo "last status $? $(hostname)" |mail -s "job done: $(pwd)" luozhenyu@genomics.cn 
